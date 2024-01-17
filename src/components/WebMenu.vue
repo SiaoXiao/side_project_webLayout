29813b0a6f2e6b7c3c8937453241345b478f0e5b
Keith <kelu0427@gmail.com>
2024-01-17 17:02:10 +0800

    feat: menu component update
    優化menu 部分程式碼

--- src/components/WebMenu.vue
+++ src/components/WebMenu.vue
@@ -6,13 +6,15 @@
       class="relative flex items-center justify-between h-[87px] md:h-auto md:py-[78px] md:justify-center md:shadow-none pr-[18px] pl-[26px]"
       :class="showMenu ? '' : 'shadow-custom'"
     >
+    <!-- mobile menu -->
       <button
-        class="flex items-center justify-center w-6 h-[18px] md:hidden"
+        class="flex-center w-6 h-[18px] md:hidden"
         @click.prevent="toggleMenu"
       >
-        <MenuIcon class="w-8 h-8" :isOpen="showMenu" />
+        <MenuIcon :isOpen="showMenu" />
       </button>
-      <div class="flex items-center justify-center">
+      <!-- Web title -->
+      <div class="flex-center">
         <span class="text-xl font-bold md:text-3xl">白頭翁不吃小米</span>
       </div>
       <!-- Logo -->
@@ -20,13 +22,13 @@
         class="static w-12 h-12 bg-white rounded-full md:-right-12 md:absolute md:w-24 md:h-24 shadow-logo"
       ></div>
     </div>
+    <!-- menu list -->
     <div
       class="flex-grow md:flex md:flex-col md:justify-between"
       :class="showMenu ? 'shadow-custom' : 'hidden'"
     >
-      <!-- 主要選單 -->
-      <ul class="flex flex-col gap-[21px] pb-[45px] sm-pb-0">
-        <li v-for="(item, i) in menuItems" :key="i" class="flex items-center justify-center">
+      <ul class="flex flex-col gap-[21px] pb-[45px] md:pb-0">
+        <li v-for="(item, i) in menuItems" :key="i" class="flex-center">
           <RouterLink
             :to="item.to"
             class="relative text-lg hover:text-[#AA6666] cursor-pointer transition ease-in-out delay-150"
@@ -47,6 +49,8 @@ const menuItems = reactive([
   { to: '/', text: '白頭翁的美照', isActive: false },
   { to: '/', text: '白頭翁的危機', isActive: false }
 ])
+// menu isActive 狀態會改用router path 判斷去做css樣式變更
+
 
 const toggleMenu = () => (showMenu.value = !showMenu.value)
 
