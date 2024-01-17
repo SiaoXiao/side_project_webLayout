60e2d74949fcb0757ce8a7682e9dbf217361b465
Keith <kelu0427@gmail.com>
2024-01-17 17:05:19 +0800

    feat: update menu icon
    優化menu icon 開關動畫

--- src/components/MenuIcon.vue
+++ src/components/MenuIcon.vue
@@ -1,15 +1,15 @@
 <template>
   <ul class="relative w-full h-full">
     <li
-      class="absolute w-full h-1 transition ease-in-out delay-150 bg-black top-1/2"
+      class="absolute w-full h-1 transition-all ease-in-out delay-150 bg-black top-1/2"
       :class="isOpen ? '-translate-y-1/2 rotate-45' : '-translate-y-2.5'"
     ></li>
     <li
-      class="absolute h-1 transition ease-in-out delay-150 bg-black top-1/2"
+      class="absolute h-1 transition-all ease-in-out delay-150 bg-black top-1/2"
       :class="isOpen ? '-translate-y-1/2 -rotate-45  w-full' : '-translate-y-1/2 w-3/4 '"
     ></li>
     <li
-      class="absolute w-full h-1 transition ease-in-out delay-150 bg-black top-1/2"
+      class="absolute w-full h-1 transition-all ease-in-out delay-150 bg-black top-1/2"
       :class="isOpen ? '-translate-y-1/2 -rotate-45' : 'translate-y-1.5'"
     ></li>
   </ul>
