ee744ccd4cb45e54e0a38e56d9224f9b6c7028ae
Keith <kelu0427@gmail.com>
2024-01-17 17:09:43 +0800

    feat: update item component
    webItem組件 部分程式優化

--- src/components/WebItem.vue
+++ src/components/WebItem.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex card h-[225px] max-w-[274px] w-full gap-9">
-    <div class="card-section pl-[5px]">
+  <div class="card">
+    <div class="card-title">
       <h2 class="relative text-4xl font-bold">
         {{ itemData.title }}
         <svg
@@ -22,7 +22,7 @@
         </svg>
       </h2>
     </div>
-    <div class="card-section">
+    <div class="card-content">
       <p class="text-base">
         {{ itemData.content }}
       </p>
